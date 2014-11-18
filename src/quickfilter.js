400f787b997ffd8d5ed278294dfe79acb6d6849f
Michael Hoy <michael.john.hoy@gmail.com>
2014-11-18 15:38:48 -0500

    finds and processes nodes

--- src/quickfilter.js
+++ src/quickfilter.js
@@ -14,11 +14,63 @@
 
   var version = "0.0.1";
 
-  var QF = function(selector) {
+  var _initializing = false;
 
+  var _hasProp = {}.hasOwnProperty;
+
+  var _old_qf = root.QF;
+
+  var QF = function(selector) {
+    this.nodes = [];
+    
+    if(!_initializing) {
+      this.selector = selector;
+      this.init();
+    }
+  };
+
+  QF.prototype = {
+    selector: undefined,
+    nodeSelector: '.node',
+
+    /* process a node (dom element), returns data object */
+    processNode: function (node) {
+      return({
+        element: node
+      });
+    },
+    
+    init: function () {
+      var q = this;
+      $(this.nodeSelector, this.selector).each(function() {
+        var data = q.processNode(this);
+        q.nodes.push(data);
+      });
+    }
+  };
+
+  QF.extend = function(prop) {
+    _initializing = true;
+    var prototype = new QF();
+    _initializing = false;
+    for (var key in prop)
+      if (_hasProp.call(prop, key))
+        prototype[key] = prop[key];
+    function K() {
+      QF.apply(this, arguments);
+    }
+    K.prototype = prototype;
+    K.prototype.constructor = K;
+
+    return K;
+  };
+
+  QF.create = function(proto) {
+    return new (QF.extend(proto))();
   };
 
   QF.version = version;
+  QF.old_qf = _old_qf;
 
   root.QF = QF;
 
--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,21 @@
 module("sanity");
 
-test("i am alive", function() {
-  equal(1,1);
+test("preserves old root QF object", function () {
+  equal(QF.old_qf, "old qf");
+});
+
+module("representation");
+
+test("finds nodes and stores elements", function() {
+  var qf = QF.create({
+    selector: "#container",
+    nodeSelector: ".node"
+  });
+
+  var node_headings = [];
+  for(var i = 0; i < qf.nodes.length; i++) {
+    var node = $(qf.nodes[i].element);
+    node_headings.push(node.find('h2').text());
+  }
+  deepEqual(node_headings, ["Node A", "Node B"]);
 });
