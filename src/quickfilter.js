73655f1ac6621a7810b24babfee4c6b97f5441ff
Michael Hoy <michael.john.hoy@gmail.com>
2014-11-18 14:57:27 -0500

    project setup

---
+++ src/quickfilter.js
@@ -0,0 +1,25 @@
+/*
+
+  quickfilter.js
+
+  (c) 2014 Michael Hoy <michael.john.hoy@gmail.com>
+
+  quickfilter may be freely distributed under the MIT license.
+
+*/
+
+(function ($, root) {
+
+  "use strict";
+
+  var version = "0.0.1";
+
+  var QF = function(selector) {
+
+  };
+
+  QF.version = version;
+
+  root.QF = QF;
+
+})(jQuery, this);
---
+++ test/tests.js
@@ -0,0 +1,5 @@
+module("sanity");
+
+test("i am alive", function() {
+  equal(1,1);
+});
